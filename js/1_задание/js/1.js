759335d1fbacc879a9fa71005c05ebda156e8721
CWDTFunny <150099171+CWDTFunny@users.noreply.github.com>
2023-12-21 12:11:53 +0900

    Add files via upload

---
+++ js/1_задание/js/1.js
@@ -0,0 +1,14 @@
+//JavaScript код с созданием переменных
+/* Автор */
+/* Дмитриев Роман Фёдорович */
+
+const myName = "Роман"
+const mySurname = "Дмитриев"
+let myFavoriteDrink = "cTE"
+let myFavoriteAnimal = "123"
+let myFavoriteProgrammingLanguage = "Html"
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
